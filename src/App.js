51749da2bb119def1d72aaa3c403091f28e832b1
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-09-01 07:16:11 +0200

    add NotFound component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 import ShowAllTables from './components/views/ShowAllTables'
 import EditTable from './components/features/EditTable'
 import Header from './components/views/Header'
+import NotFound from './components/pages/NotFound'
 
 const App = () => {
 	const dispatch = useDispatch()
@@ -19,6 +20,7 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<ShowAllTables />}></Route>
 				<Route path='/table/:id' element={<EditTable />}></Route>
+				<Route path='/*' element={<NotFound />}></Route>
 			</Routes>
 		</Container>
 	)
